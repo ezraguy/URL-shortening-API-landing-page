a0566b02bf2b6020d5b725b5a64d903b8a734ea5
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-09-20 19:12:31 +0300

    ui changes

--- src/components/url-section.jsx
+++ src/components/url-section.jsx
@@ -4,21 +4,35 @@ import '../scss/url-section.scss';
 const UrlSection = () => {
 
     const [url, setUrl] = useState('');
+    const [urlArr, setUrlArr] = useState([])
     const [error, setError] = useState(false)
     const handleClick = () => {
         if (url === "") {
             setError(true)
-        } else
+        } else {
             setError(false)
+            setUrlArr(oldArr => [...oldArr, url]);
+            setUrl('');
+        }
     }
     return (
-        <div className="url-section">
+        <div className="url-section-wrap">
+            <div className="url-section">
+                <input type="text" onChange={(e) => setUrl(e.target.value)} className={error ? "url-input error" : " url-input"} placeholder="Shorten a link here..." value={url} />
+                <button className="shorten-btn" onClick={handleClick}>Shorten it!</button>
 
-            <input type="text" onChange={(e) => setUrl(e.target.value)} className={error ? "url-input error" : " url-input"} placeholder="Shorten a link here..." />
-            <button className="shorten-btn" onClick={handleClick}>Shorten it!</button>
+                {error && <span className="err-message">Please add a link</span>}
 
-            {error && <span className="err-message">Please add a link</span>}
-        </div >
+
+            </div >
+            <div className="urls">
+                {urlArr.map((url, index) => {
+                    return (
+                        <div className="url" key={index}>{url}</div>
+                    )
+                })}
+            </div>
+        </div>
     );
 }
 
